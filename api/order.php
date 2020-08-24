03707a0c0996a43ddf5a8eccdbfa15096004b11d
張 建文 <chang180@gmail.com>
2020-08-24 08:22:00 +0800

    修改訂票編號方式

--- api/order.php
+++ api/order.php
@@ -2,7 +2,8 @@
 include_once "../base.php";
 $_POST['qt']=count($_POST['seat']);
 $_POST['seat']=serialize($_POST['seat']);
-$mno=$Ord->q("SELECT MAX(no) FROM ord ")[0][0]+1;
+$id=$Ord->q("SELECT MAX(id) FROM ord ")[0][0]+1;
+$mno=date("Ymd").sprintf("%04d",$id);
 $_POST['no']=$mno;
 $Ord->save($_POST);
 echo $mno;
